0aa90681489541f9804523cbc467aacdfc597ea2
amenlouis <loamen28@gmail.com>
2023-03-16 01:05:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main . A progress with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
